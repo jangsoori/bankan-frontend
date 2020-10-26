80d260e587da862ab6ccc67df11ed73786cea234
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-26 16:56:58 +0000

    Modified forms and connected with backend routes

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,6 @@
-import styled from "@emotion/styled";
 import React from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
 import Logo from "../common/Logo";
+import Cookies from "js-cookie";
 import {
   Inputs,
   InputWrapper,
@@ -18,36 +16,120 @@ import {
   StyledForm,
   Wrapper,
 } from "../../utils/forms";
+import { toast, ToastContainer } from "react-toastify";
+import { Formik } from "formik";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
 
 export default function Login({ title, children, values }) {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const history = useHistory();
+  const onSubmit = (data) => {
+    //If all good, proceed to creating user
+    Axios.post(
+      "/api/auth/authUser",
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }
+    )
+      .then((res) => {
+        toast.success("Successful login! Redirecting...");
+        Cookies.get("token") && history.push("/");
+      })
+      .catch((e) => {
+        toast.error(e.response.data.msg);
+      });
+  };
+
   return (
     <Wrapper>
       <Logo />
-      <StyledForm onSubmit={handleSubmit(onSubmit)}>
-        <Title>Login</Title>
-        <Inputs>
-          <InputWrapper>
-            <Label htmlFor="login_email">Email</Label>
-            <Input name="login_email" id="login_email" ref={register} />
-          </InputWrapper>
-          <InputWrapper>
-            <Label htmlFor="login_password">Password</Label>
-            <Input
-              name="login_password"
-              id="login_password"
-              type="password"
-              ref={register}
-            />
-          </InputWrapper>
-        </Inputs>
-        <Submit type="submit" value="Go in!" />
-        <Meta>
-          <MetaItem to="/forgot-password">Forgot password?</MetaItem>
-          <MetaItem to="/signup">Don't have an account? Sign up</MetaItem>
-        </Meta>
-      </StyledForm>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          //Handle email input
+          if (!values.email) {
+            errors.email = "Email is required!";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address!";
+          }
+
+          //Handle passwords input
+
+          if (!values.password) {
+            errors.password = "Password is required!";
+          }
+
+          return errors;
+        }}
+        onSubmit={onSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <StyledForm onSubmit={handleSubmit}>
+            <Inputs>
+              <InputWrapper>
+                <Label>Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <Error>{errors.email && touched.email && errors.email}</Error>
+              </InputWrapper>
+              <InputWrapper>
+                <Label>Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <Error>
+                  {errors.password && touched.password && errors.password}
+                </Error>
+              </InputWrapper>
+            </Inputs>
+            {isSubmitting ? (
+              <Submit type="submit" value="Loggin in..." />
+            ) : (
+              <Submit type="submit" value="Log in!" />
+            )}
+          </StyledForm>
+        )}
+      </Formik>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Wrapper>
   );
 }
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import Logo from "../common/Logo";
@@ -18,44 +18,151 @@ import {
   StyledForm,
   Wrapper,
 } from "../../utils/forms";
+import Axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Formik, useFormik } from "formik";
 
-export default function Signup({ title, children, values }) {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+export default function Signup() {
+  const onSubmit = (data) => {
+    console.log(data);
+    if (data.password !== data.repeat_password) {
+      console.log("this dont maczz");
+    }
+    //If all good, proceed to creating user
+    Axios.post(
+      "/api/auth/createUser",
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }
+    )
+      .then((res) => {
+        toast.success("Account created successfully! You can now log in.");
+      })
+      .catch((e) => {
+        toast.error(e.response.data.msg);
+      });
+  };
+
   return (
     <Wrapper>
       <Logo />
-      <StyledForm onSubmit={handleSubmit(onSubmit)}>
-        <Title>Sign up</Title>
-        <Inputs>
-          <InputWrapper>
-            <Label htmlFor="sign_up_email">Email</Label>
-            <Input name="sign_up_email" id="sign_up_email" ref={register} />
-          </InputWrapper>
-          <InputWrapper>
-            <Label htmlFor="sign_up_password">Password</Label>
-            <Input
-              name="sign_up_password"
-              id="sign_up_password"
-              type="password"
-              ref={register}
-            />
-          </InputWrapper>
-          <InputWrapper>
-            <Label htmlFor="sign_up_repeat_password">Password</Label>
-            <Input
-              name="sign_up_repeat_password"
-              id="sign_up_repeat_password"
-              type="password"
-              ref={register}
-            />
-          </InputWrapper>
-        </Inputs>
-        <Submit type="submit" value="Create an account" />
-        <Meta>
-          <MetaItem to="/login">Already have an account? Log in</MetaItem>
-        </Meta>
-      </StyledForm>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          repeat_password: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          //Handle email input
+          if (!values.email) {
+            errors.email = "Email is required!";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address!";
+          }
+
+          //Handle passwords input
+
+          if (!values.password) {
+            errors.password = "Password is required!";
+          }
+
+          if (values.password !== values.repeat_password) {
+            errors.repeat_password = "Passwords do not match!";
+          }
+          if (values.password && !values.repeat_password) {
+            errors.repeat_password = "Repeat your password!";
+          }
+          if (values.password.length < 6) {
+            errors.password =
+              "Please provide password that is actually somewhat good...";
+          }
+          return errors;
+        }}
+        onSubmit={onSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <StyledForm onSubmit={handleSubmit}>
+            <Inputs>
+              <InputWrapper>
+                <Label>Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <Error>{errors.email && touched.email && errors.email}</Error>
+              </InputWrapper>
+              <InputWrapper>
+                <Label>Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <Error>
+                  {errors.password && touched.password && errors.password}
+                </Error>
+              </InputWrapper>
+              <InputWrapper>
+                <Label>Repeat password</Label>
+                <Input
+                  type="password"
+                  name="repeat_password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.repeat_password}
+                />
+                <Error>
+                  {errors.repeat_password &&
+                    touched.repeat_password &&
+                    errors.repeat_password}
+                </Error>
+              </InputWrapper>
+            </Inputs>
+            {isSubmitting ? (
+              <Submit type="submit" value="Submitting..." />
+            ) : (
+              <Submit type="submit" value="Sign up!" />
+            )}
+            <Meta>
+              <MetaItem to="/login">Already registered?</MetaItem>
+            </Meta>
+          </StyledForm>
+        )}
+      </Formik>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Wrapper>
   );
 }
