44ffa0e0f6c1eb26c6baa324d4883a35d9698a1a
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-26 16:57:16 +0000

    Add context

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,24 @@ import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
 import ResetPassPage from "../pages/ResetPassPage";
-
+import useAuth, { AuthContext } from "../hooks/useAuth";
+import Cookies from "js-cookie";
 const StyledApp = styled.main`
   height: 100%;
 `;
 export default function App() {
+  const user = useAuth();
+
   return (
-    <StyledApp>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/signup" component={SignupPage} />
-        <Route exact path="/forgot-password" component={ResetPassPage} />
-      </Switch>
-    </StyledApp>
+    <AuthContext.Provider value={user}>
+      <StyledApp>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignupPage} />
+          <Route exact path="/forgot-password" component={ResetPassPage} />
+        </Switch>
+      </StyledApp>
+    </AuthContext.Provider>
   );
 }
