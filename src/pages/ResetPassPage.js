4fac92210235a5d98868600f4bbc4c59fe38aff9
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-27 14:09:56 +0000

    Change function name

--- src/pages/ResetPassPage.js
+++ src/pages/ResetPassPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Reset from "../components/auth/Reset";
 
-export default function Signup() {
+export default function ResetPasswordPage() {
   return <Reset />;
 }
