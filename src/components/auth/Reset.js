7f578a619d035b887d2bbd849fe35553a0b12bfc
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-27 14:09:43 +0000

    Change reset  form

--- src/components/auth/Reset.js
+++ src/components/auth/Reset.js
@@ -1,7 +1,5 @@
-import styled from "@emotion/styled";
 import React from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+
 import Logo from "../common/Logo";
 import {
   Inputs,
@@ -18,26 +16,67 @@ import {
   StyledForm,
   Wrapper,
 } from "../../utils/forms";
+import { Formik } from "formik";
 
-export default function Signup({ title, children, values }) {
-  const { register, handleSubmit, watch, errors } = useForm();
+export default function ResetPassword() {
   const onSubmit = (data) => console.log(data);
   return (
     <Wrapper>
       <Logo />
-      <StyledForm onSubmit={handleSubmit(onSubmit)}>
-        <Title>Reset password</Title>
-        <Inputs>
-          <InputWrapper>
-            <Label htmlFor="resetemail">Email</Label>
-            <Input name="resetemail" id="resetemail" ref={register} />
-          </InputWrapper>
-        </Inputs>
-        <Submit type="submit" value="Reset password" />
-        <Meta>
-          <MetaItem to="/login">Log in</MetaItem>
-        </Meta>
-      </StyledForm>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          //Handle email input
+          if (!values.email) {
+            errors.email = "Email is required!";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address!";
+          }
+
+          //Handle passwords input
+
+          if (!values.password) {
+            errors.password = "Password is required!";
+          }
+
+          return errors;
+        }}
+        onSubmit={onSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <StyledForm onSubmit={handleSubmit}>
+            <Title>Login</Title>
+            <Inputs>
+              <InputWrapper>
+                <Label>Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <Error>{errors.email && touched.email && errors.email}</Error>
+              </InputWrapper>
+            </Inputs>
+            <Submit type="submit" value="Get new password" />
+          </StyledForm>
+        )}
+      </Formik>
     </Wrapper>
   );
 }
