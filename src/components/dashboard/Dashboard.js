f25af89177ed21153148cbb849c2c2de03c87379
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-27 18:59:10 +0000

    Added Content to dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,20 +3,28 @@ import styled from "@emotion/styled";
 import SideNav from "./SideNav";
 import { Route, Switch } from "react-router-dom";
 import Profile from "./content/Profile";
-import Main from "./content/Main";
+import Main from "./content/Home";
 
 const StyledDashboard = styled.section`
   display: flex;
   height: 100%;
 `;
+const StyledContent = styled.section`
+  padding: 3rem;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  row-gap: 4rem;
+`;
 export default function Dashboard() {
   return (
     <StyledDashboard>
       <SideNav />
-      <Switch>
-        <Route exact path="/dashboard/profile" component={Profile} />
-        <Route exact path="/dashboard" component={Main} />
-      </Switch>
+      <StyledContent>
+        <Switch>
+          <Route exact path="/dashboard/profile" component={Profile} />
+          <Route exact path="/dashboard" component={Main} />
+        </Switch>
+      </StyledContent>
     </StyledDashboard>
   );
 }
