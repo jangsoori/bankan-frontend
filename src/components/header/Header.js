079c3ff20fa1cd5b1030653dff51376fed5afc2a
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-27 14:03:51 +0000

    Fixed errors

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
 import styled from "@emotion/styled";
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import useAuth, { AuthContext } from "../../hooks/useAuth";
+import React, { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Logo from "../common/Logo";
+import Cookies from "js-cookie";
+import UserContext from "../../context/UserContext";
+import useAuth from "../../hooks/useAuth";
 
+//!Styling
 const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
@@ -17,30 +20,41 @@ const StyledNav = styled.nav`
   grid-auto-flow: column;
 `;
 
-const NavItem = styled(Link)`
+const NavItem = styled.button`
   border-radius: 5px;
   color: ${({ theme }) => theme.colors.white};
   font-size: 2rem;
   padding: 1rem 2rem;
+  border: none;
+  background: none;
+  cursor: pointer;
   &:hover {
     background-color: rgba(255, 255, 255, 0.1);
   }
 `;
+
+//!main
 export default function Header() {
-  const user = useContext(AuthContext);
+  const user = useContext(UserContext);
+  const { logout } = useAuth();
+  const history = useHistory();
+
+  console.log(user);
   return (
     <StyledHeader>
       <StyledLogo />
       <StyledNav>
-        {!user ? (
+        {user.userData.token && user.userData.user ? (
           <>
-            <NavItem to="/login">Log in</NavItem>
-            <NavItem to="/signup">Sign up</NavItem>
+            <NavItem onClick={() => history.push("/")}>Dashboard</NavItem>
+            <NavItem onClick={() => logout()} to="/">
+              Log out
+            </NavItem>
           </>
         ) : (
           <>
-            <NavItem to="/login">Dashboard</NavItem>
-            <NavItem to="/signup">Log out</NavItem>
+            <NavItem onClick={() => history.push("/login")}>Log in</NavItem>
+            <NavItem onClick={() => history.push("/signup")}>Sign up</NavItem>
           </>
         )}
       </StyledNav>
